00ab7cf86540b99c2dce6d6991e706367c1ec8e2
glodzienski <crystopher.glodzienski@gmail.com>
2019-05-01 17:13:55 -0300

    - Criado classe de entidade User.java

---
+++ src/main/java/webbook/api/entity/User.java
@@ -0,0 +1,44 @@
+package webbook.api.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotNull
+    @Column(nullable = false, unique = true)
+    private String code;
+
+    @NotNull
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @NotNull
+    @Column(nullable = false)
+    private String password;
+
+    @NotNull
+    @Column(nullable = false)
+    private String cpf;
+
+    @NotNull
+    @Column(name = "birthday_date", nullable = false)
+    private String birthdayDate;
+
+    @NotNull
+    @Column(nullable = false)
+    private String name;
+
+    @NotNull
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @NotNull
+    @Column(name = "photo_url")
+    private String photoUrl;
+}
